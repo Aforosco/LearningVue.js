9e273bbd7185fec1171378a17ecfa7cf3168d18d
aforosco <oregbemijohn77@gmail.com>
2024-05-09 16:42:53 +0100

    Added HomePage.vue and modified settings

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,65 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo"/>
+            Build-a-bot
+          </li>
+        </ul>
+      </nav>
+    </header>
+  <!--<HomePage msg ="Welcome to your vue.js app"/>-->
+  <RobotBuilder/>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    RobotBuilder,
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Avenir,Helvetica,Arial,sans-serif;
 }
-</style>
+body{
+  background: linear-gradient(to bottom, #555 ,#999);
+  background-attachment: fixed;
+}
+main{
+  margin: 10px auto;
+  padding: 30px;
+  background-color: white;
+  width: 1400px;
+  min-height: 300px;
+}
+header{
+  background-color: #999;
+  width: 1300px;
+  margin: 0 auto;
+}
+
+ul {
+ display: flex;
+ padding: 3px;
+}
+
+.nav-item {
+ display: inline-block;
+ padding: 5px 10px;
+ font-size: 20px;
+ border-right: 1px solid #bbb;
+}
+.logo{
+  vertical-align: middle;
+  height: 30px;
+}
+</style>./home/HomePage.vue./HomePage.vue
